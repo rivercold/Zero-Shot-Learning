9ae39345e10eba8c070d4a878b62cce7d94f9b14
Desai Fan <dfan@andrew.cmu.edu>
2016-11-06 19:24:46 -0500

    Fix evaluate.py, again

--- evaluate.py
+++ evaluate.py
@@ -10,23 +10,29 @@ def get_metrics(predictions, labels):
     class_num = predictions.shape[1]
 
     true_labels = np.zeros(shape=predictions.shape)
-    true_labels[xrange(test_size), labels] = 1
+    true_labels[xrange(test_size), labels.T] = 1
 
     ## roc_auc and ap
-    roc_auc = np.zeros(shape=(1, class_num))
-    ap = np.zeros(shape=(1, class_num))
+    roc_auc = np.zeros(shape=(class_num))
+    ap = np.zeros(shape=(class_num))
 
     for i in xrange(class_num):
+        # Might cause error: ValueError: Only one class present in y_true. ROC AUC score is not defined in that case.
         roc_auc[i] = roc_auc_score(true_labels[:, i], predictions[:, i]) # default macro
         ap[i] = average_precision_score(true_labels[:, i], predictions[:, i]) # default macro
 
 
     ## top 1 and top 5 accuracy
-    top1 = np.argmax(predictions, axis=1)
+    top1 = np.argmax(predictions, axis=1)[np.newaxis].T
     top1_accu = np.sum(top1 == labels) / float(test_size)
 
     top5 = np.argsort(predictions, axis=1)[:, -5:]
-    top5_accu = np.sum(top5 == label) / float(test_size)
+    top5_accu = np.sum(top5 == labels) / float(test_size)
 
     return (roc_auc, ap, top1_accu, top5_accu)
     
+if __name__ == '__main__':
+    predictions = np.random.uniform(size=(1000,10))
+    labels = np.random.randint(10, size=(1000,1))
+
+    get_metrics(predictions, labels)
