aa9a4b6bec76eef095f267ad5dc563fba874f962
yuhongliang324 <yuhongliang324@gmail.com>
2016-12-04 17:17:52 -0500

    revise train

--- zsl/train.py
+++ zsl/train.py
@@ -199,7 +199,7 @@ def test1():
 
     V_train, Y_train, T_train, V_seen, Y_seen, T_seen, V_unseen, Y_unseen, T_unseen\
         = load_data.prepare_data(matroot, split_file, unseen_file, wiki_npy)
-    print V_train.shape, Y_train.shape
+    print V_train.shape, Y_train.shape, T_train.shape
     start_time = timeit.default_timer()
     train(V_train, Y_train, T_train, V_seen, Y_seen, T_seen, V_unseen, Y_unseen, T_unseen, obj='Hinge')
     end_time = timeit.default_timer()
