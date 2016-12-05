6f02e9d270d86772d665757a588ed4a2ac38f9cf
Desai Fan <dfan@andrew.cmu.edu>
2016-12-04 21:30:30 -0500

    Filter certain sections.

--- wiki_url/api_extracted/fetch_text.py
+++ wiki_url/api_extracted/fetch_text.py
@@ -36,14 +36,48 @@ def extract_section():
             # all_sections = all_sections.union(sections)
 
     # print all_sections
-    with open('section.txt', 'w') as f_full:
+    with open('all_sections.txt', 'w') as f_full:
         for w in sorted(all_sections, key=all_sections.get, reverse=True):
             # print w, d[w]
             f_full.write('%d %s' % (all_sections[w], w) + '\n')
 
 
+def filter_text():
+    regex = r' ={2,4} [\w ]+ ={2,4} '
+    regex_title = r' ={2,4} ([\w ]+) ={2,4} '
+
+    filtered_titles = set()
+    with open('filtered_titles.txt', 'r') as f:
+        for line in f:
+            filtered_titles.add(line.rstrip())
+
+    all_sections = dict()
+
+    with open('full_filtered.txt', 'w') as f_writer:
+        with open('full.txt', 'r') as f:
+            for line in f:
+                cur_sections = re.findall(regex_title, line)
+                cur_contents = re.split(regex, line.rstrip())
+
+                for idx, s in enumerate(cur_sections):
+                    if s in filtered_titles:
+                        cur_contents[idx + 1] = ''
+
+                new_contents = ' '.join(cur_contents)
+                f_writer.write(new_contents + '\n')
+                
+                
+
+    # print all_sections
+    with open('all_sections.txt', 'w') as f_full:
+        for w in sorted(all_sections, key=all_sections.get, reverse=True):
+            # print w, d[w]
+            f_full.write('%d %s' % (all_sections[w], w) + '\n')
+
 
 if __name__ == '__main__':
     # extract_text()
 
-    extract_section()
+    # extract_section()
+
+    filter_text()
